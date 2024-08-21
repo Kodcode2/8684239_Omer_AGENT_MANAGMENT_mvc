2bb9302af8269449b02f5e771a1044e4e39c81b1
ovadia-nisimov <ovadianisimov@gmail.com>
2024-08-21 17:53:17 +0300

    ייי

---
+++ Management_of_Mossad_agents---MVC/Management_of_Mossad_agents---MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Management_of_Mossad_agents___MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
